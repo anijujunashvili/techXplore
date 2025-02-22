84e81415046a69e904fbad8ff9271f67ac737711
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2025-02-22 21:47:12 +0400

    change not found page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,5 +1,13 @@
 const NotFountPage = () => {
-  return <>404</>;
+  return (
+    <>
+      <div>
+        <div className="w-4/5 mx-auto mt-11 font-primaryBold text-6xl text-red-700 text-center">
+          404
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFountPage;
